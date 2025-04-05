3147520bce1ebcf705a556ef84afc86ed4656ddf
Jesscha <jessecha222@gmail.com>
2025-04-06 02:39:42 +0900

    Remove unused import

--- programs/trade-dot-fun/src/instructions/distribute_zbtc_reward.rs
+++ programs/trade-dot-fun/src/instructions/distribute_zbtc_reward.rs
@@ -7,13 +7,13 @@ use anchor_spl::{
     associated_token::get_associated_token_address,
     token_interface::{Mint, TokenAccount, TokenInterface, Transfer, Burn}
 };
-use pyth_solana_receiver_sdk::price_update::{get_feed_id_from_hex, PriceUpdateV2};
+use pyth_solana_receiver_sdk::price_update::PriceUpdateV2;
 
 use crate::{
     state::{Round, RoundState, VaultData},
     errors::RoundError,
-    utils::{inf_mint, zbtc_mint, find_vault_authority_pda, find_round_pda, chip_token_mint, 
-            INF_USD_PRICE_FEED_ID, SOL_USD_PRICE_FEED_ID, MAXIMUM_AGE, check_authorized_admin,
+    utils::{ zbtc_mint, find_vault_authority_pda, find_round_pda, chip_token_mint, 
+             check_authorized_admin,
             jupiter_program_id},
 };
 
